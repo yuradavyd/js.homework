b713a3a0295e297cdd42f6973235f46bc6448e8e
yuradavyd <yurko.davyd@gmail.com>
2021-11-11 16:22:36 +0200

    msg

---
+++ lesson1/homework1/main.js
@@ -0,0 +1,89 @@
+// - Створити змінні. Присвоїти кожному з них значення:
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let hello = 'hello';
+// let owu = 'owu';
+// let com = 'com';
+// let ua = 'ua';
+// let one = 1;
+// let ten = 10;
+// let minus_nine = -999;
+// let onetwothree = 123;
+// let PI = 3.14;
+// let twodotseven = 2.7;
+// let sixteen = 16;
+// let pravda = true;
+// let brehnya = false;
+//
+// console.log(hello, owu, com, ua, one, ten, minus_nine, onetwothree, PI, twodotseven, sixteen, pravda, brehnya);
+// alert(`${hello},${owu},${com},${ua},${one},${ten},${minus_nine},${onetwothree},${PI},${twodotseven},${sixteen},${pravda},${brehnya}`)
+// document.write(`${hello},${owu},${com},${ua},${one},${ten},${minus_nine},${onetwothree},${PI},${twodotseven},${sixteen},${pravda},${brehnya}`)
+//
+// // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+//
+// hello = 'hi';
+// owu = 'okten';
+// com = 'net';
+// ua = 'lviv';
+// one = 'one';
+// ten = 'ten';
+// minus_nine = '-999';
+// onetwothree = 'onetwothree';
+// PI = '3.14';
+// twodotseven = 'twodotseven';
+// sixteen = 'sixteen';
+// pravda = 'true';
+// brehnya = 'false';
+//
+// console.log(hello, owu, com, ua, one, ten, minus_nine, onetwothree, PI, twodotseven, sixteen, pravda, brehnya);
+// alert(`${hello},${owu},${com},${ua},${one},${ten},${minus_nine},${onetwothree},${PI},${twodotseven},${sixteen},${pravda},${brehnya}`)
+// document.write(`${hello},${owu},${com},${ua},${one},${ten},${minus_nine},${onetwothree},${PI},${twodotseven},${sixteen},${pravda},${brehnya}`)
+//
+// // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+// // З'єднати їх в одну змінну person. (Не об'єкт)
+//
+// let firstName = 'Yura';
+// let middleName = 'Ihorovych';
+// let lastName = 'Davyd';
+//
+// let person = firstName + middleName + lastName;
+//
+// // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// //     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+//
+// let firstname = prompt("введіть своє ім'я");
+// let middlename = prompt('Введіть своє по-батькові');
+// let age = prompt('Вкажіть свій вік');
+// alert('Вітаю ' + firstname + ' ' + middlename + '. Тобі ' + age + ' років.');
+//
+// // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// console.log(typeof a);
+// let b = '100';
+// console.log(typeof b);
+// let c = true;
+// console.log(typeof c);
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+
+// 5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+
+// console.log(5<6, 5>6, 5===6, 5>=6, 10===10, 10>=10, 10>10, 10<10, 10!=10, 123==='123', 123=='123');
+
+
+
