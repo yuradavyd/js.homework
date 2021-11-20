f64f31302ccc0b913393276e49c891855d21e4f6
yuradavyd <yurko.davyd@gmail.com>
2021-11-20 12:58:21 +0200

    msg

--- lesson5/classwork5/main.js
+++ lesson5/classwork5/main.js
@@ -1,6 +1,6 @@
 // - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
-
-// function num3(a,b,c) {
+//
+// let num3 = (a,b,c) => {
 //     if (a < b && a < c) {
 //         console.log(a);
 //     } else if(b < c) {
@@ -14,7 +14,7 @@
 // - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
 
 //
-// function num3(a,b,c) {
+// let num3 = (a,b,c) => {
 //     if (a > b && a > c) {
 //         console.log(a);
 //     } else if(b > c) {
@@ -26,8 +26,8 @@
 // num3(1,2,3)
 
 // - створити функцію яка повертає найбільше число з масиву
-
-// function arr_max(array) {
+//
+// let arr_max = (array) => {
 //     let x = array[0]
 //     for (let i = 0; i < array.length; i++) {
 //        if (array[i] > x) {
@@ -40,7 +40,7 @@
 
 // - створити функцію яка повертає найменьше число з масиву
 
-// function arr_min(array) {
+// let arr_min = (array) => {
 //     let x = array[0]
 //     for (let i = 0; i < array.length; i++) {
 //        if (array[i] < x) {
@@ -53,7 +53,7 @@
 
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
 
-// function arr_sum(array) {
+// let arr_sum = (array) => {
 //     let sum = 0;
 //     for (let i = 0; i < array.length; i++) {
 //         sum += array[i]
@@ -66,7 +66,7 @@
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
 
 
-// function arr_ser(array) {
+// let arr_ser = (array) => {
 //     let sum = 0;
 //     for (let i = 0; i < array.length; i++) {
 //         sum += array[i]
@@ -77,8 +77,8 @@
 // arr_ser([1,2,3,4,5])
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
-
-// function num_minmax() {
+//
+// let num_minmax = (...arguments) =>  {
 //     let max = arguments[0]
 //     let min = arguments[0]
 //     for (let i = 0; i < arguments.length; i++) {
@@ -94,8 +94,8 @@
 // num_minmax(1,2,3,4,5)
 
 // - створити функцію яка заповнює масив рандомними числами
-
-// function fill(array) {
+//
+// let fill = (array) => {
 //     for (let i = 0; i < 100; i++) {
 //         array[i] = Math.round(Math.random()*100)
 //     }
@@ -104,8 +104,8 @@
 // fill([])
 
 // - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
-
-// function fill(array,limit) {
+//
+// let fill = (array,limit) => {
 //     for (let i = 0; i < limit; i++) {
 //         array[i] = Math.round(Math.random()*100)
 //     }
@@ -116,7 +116,7 @@
 
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
 //
-// function reverse(array) {
+// let reverse = ((array) => {
 //     let y = [];
 //     let x = 0;
 //     for (let i = array.length - 1; i >= 0; i--) {
@@ -124,5 +124,5 @@
 //          x++
 //     }
 //     console.log(y)
-// }
-// reverse([1,2,3])
+// })([1,2,3])
+
